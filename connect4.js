e456b8edf9f54469f013a19203ba92fadb1d8361
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-23 17:36:32 -0500

    Add comments for readability and three methods to Game class

--- connect4.js
+++ connect4.js
@@ -6,13 +6,16 @@
  */
 
 class Game {
-  constructor(HEIGHT, WIDTH) {
-    this.HEIGHT = 6;
-    this.WIDTH = 7;
-    this.boardInMemory = [];
-    this.currPlayer = 1;
+  constructor(height = 6, width = 7) {
+    this.HEIGHT = height;
+    this.WIDTH = width;
+    this.boardInMemory = []; // array of rows, each row is array of cells  (board[y][x])
+    this.currPlayer = 1; // active player: 1 or 2
     this.allowTurn = true;
     this.htmlBoard = document.getElementById("htmlBoard");
+    /** makeBoard: create in-JS board structure:
+     *   board = array of rows, each row is array of cells  (board[y][x])
+     */
     this.makeBoard();
     this.makeHtmlBoard();
   }
@@ -21,6 +24,7 @@ class Game {
       this.boardInMemory.push(Array.from({ length: this.WIDTH }));
     }
   }
+  /** makeHtmlBoard: make HTML table and row of column tops. */
   makeHtmlBoard() {
     // make column tops (clickable area for adding a piece to that column)
     const top = document.createElement("tr");
@@ -47,6 +51,29 @@ class Game {
 
       this.htmlBoard.append(row);
     }
+  }
+  /** findSpotForCol: given column x, return top empty y (null if filled) */
+  findSpotForCol(x) {
+    for (let y = this.HEIGHT - 1; y >= 0; y--) {
+      if (!board[y][x]) {
+        return y;
+      }
+    }
+    return null;
+  }
+  /** placeInTable: update DOM to place piece into HTML table of board */
+  placeInTable(y, x) {
+    const piece = document.createElement("div");
+    piece.classList.add("piece");
+    piece.classList.add(`p${currPlayer}`);
+    piece.style.top = -50 * (y + 2);
+
+    const spot = document.getElementById(`${y}-${x}`);
+    spot.append(piece);
+  }
+  /** endGame: announce game end */
+  endGame(msg) {
+    alert(msg);
   }
 }
 
